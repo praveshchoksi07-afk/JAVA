f7a404d4b105f957849d6b8a6e5d5422d8055ef3
praveshchoksi07-afk <praveshchoksi07@gmail.com>
2026-07-08 21:03:03 +0530

    first commit

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class ArrayList { 
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("whats your age");
+        int age=sc.nextInt();
+        sc.nextLine();
+        System.out.println("Whats your name");
+        String name=sc.nextLine();
+        
+        System.out.println("your name is "+ name);
+        System.out.println("your age is"+age);
+    }
+}
+
+ 
+    
+
